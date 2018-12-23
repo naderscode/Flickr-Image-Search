c5b61d8a50f037cb52f0ce2c230a5304b19634f9
naderscode <naderscode@gmail.com>
2018-12-23 01:26:11 -0500

    insert images into the DOM

--- javascripts/app.js
+++ javascripts/app.js
@@ -4,9 +4,22 @@ var main = function(){
 	var url = "http://api.flickr.com/services/feeds/photos_public.gne?tags=flowers&format=json&jsoncallback=?";
 	$.getJSON(url, function(flickrResponse){
 
-		console.log(flickrResponse);
+		//iterate over the item objects
+		flickrResponse.items.forEach(function(photo){
+			
+			// print out individual images
+			//console.log(photo.media.m);
 
+			var $photos = $(".photos");
+
+			var $image = $("<img>");
+			
+			//add image src attribute using jQuery 
+			$image.attr("src", photo.media.m);
+				//append to class .photos
+				$photos.append($image);
+		});
 	});
-}
+};
 
 $(document).ready(main);
