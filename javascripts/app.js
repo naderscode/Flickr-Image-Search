f90731be11c25b83dd1fc8f5881186051ad1a259
naderscode <naderscode@gmail.com>
2018-12-23 00:38:35 -0500

    create function to get JSON objects from Flickr

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,7 +1,12 @@
 var main = function(){
 	"use strict";
 
-	
+	var url = "http://api.flickr.com/services/feeds/photos_public.gne?tags=flowers&format=json&jsoncallback=?";
+	$.getJSON(url, function(flickrResponse){
+
+		console.log(flickrResponse);
+
+	});
 }
 
 $(document).ready(main);
