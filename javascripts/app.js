45f9282e6b9a52232f5e896516953c269e0abde6
naderscode <naderscode@gmail.com>
2018-12-25 01:56:01 -0500

    fix indentation in app.js

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,56 +1,46 @@
+"use strict";
 
-var main = function(){
-	"use strict";
-
+var main = function() {
 	var $input = $(".input");
 	var $button = $(".search");
 	
 	var searchURL;
 
-	$button.on("click", function(event){
+	$button.on("click", function(event) {
 
 		if($input.val() !== "") {
-
-			$(".photos").empty();
-		
 			var $searchTag=$input.val();
+			$(".photos").empty();
 
 			//console.log($searchTag);
-		
-			
 			var searchURL = "http://api.flickr.com/services/feeds/photos_public.gne?tags="+$searchTag+"&format=json&jsoncallback=?";
 				
 			//console.log(searchURL);
 			
 			$input.val("");
 
-
-			$.getJSON(searchURL, function(flickrResponse){
-
-		//iterate over the item objects
-		flickrResponse.items.forEach(function(photo){
-			
-			// print out individual images
-			//console.log(photo.media.m);
-
-			var $photos = $(".photos");
-
-			var $image = $("<img>").hide();
-			
-			//add image src attribute to each returned url  
-			$image.attr("src", photo.media.m);
-				//append to class .photos
-				$photos.append($image);
-				$image.fadeIn();
-		});
-	});
-
+			$.getJSON(searchURL, function(flickrResponse) {
+				//iterate over the item objects
+				flickrResponse.items.forEach(function(photo) {
+					// print out individual images
+					//console.log(photo.media.m);
+					var $photos = $(".photos");
+					var $image = $("<img>").hide();
+					
+					//add image src attribute to each returned url  
+					$image.attr("src", photo.media.m);
+					
+					//append to class .photos
+					$photos.append($image);
+					$image.fadeIn();
+				});
+			});
 		}
 	});
-	
 };
 
 $(document).ready(main);
+
 
 
 
